9a2329360855d2060b21e59a975b02a096906297
timonen87 <71833451+timonen87@users.noreply.github.com>
2020-10-05 14:27:52 +0300

    Update lesson3_1.py

--- lesson3_1.py
+++ lesson3_1.py
@@ -1,7 +1,6 @@
 def func_calc(*args):
     var_1 = int(input("Введите первое число "))
     var_2 = int(input("Введите второне число "))
-    var_3 = var_1 / var_2
     if var_2 != 0:
         return var_1 / var_2
     else:
