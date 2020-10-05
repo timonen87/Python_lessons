9b46a47d741a037546b27f31241d8a734632af23
timonen87 <71833451+timonen87@users.noreply.github.com>
2020-10-05 14:25:36 +0300

    Update lesson3_1.py

--- lesson3_1.py
+++ lesson3_1.py
@@ -1,25 +1,24 @@
-
-
 def func_calc(*args):
+    var_1 = int(input("Введите первое число "))
+    var_2 = int(input("Введите второне число "))
+    res = var_1 / var_2
+    if var_2 != 0:
+        return var_1 / var_2
+    else:
+        print("На ноль делить нельзя!")
 
+    '''
     try:
         var_1 = int(input("Введите первое число "))
         var_2 = int(input("Введите второне число "))
-        res = var_1 / var_2
+        var_3 = var_1 / var_2
     except ValueError:
         return 'Value error'
     except ZeroDivisionError:
         return "На ноль делить нельзя!"
 
-    return res
-    
-    '''
-    вариант через if
-    
-    if var_2 != 0:
-        return var_1 / var_2
-    else:
-        print("На ноль делить нельзя!")
+    return var_3
+
     '''
 
 
