b79cf1a3fe895b31276973cae7b69c1be3fb9db9
timonen87 <71833451+timonen87@users.noreply.github.com>
2020-10-10 20:40:49 +0300

    Update Lesson4_2.py

--- Lesson4_2.py
+++ Lesson4_2.py
@@ -7,5 +7,5 @@
 
 my_list = [44, 4, 7, 1, 18, 5, 9, 11]
 my_new_list = [el for num, el in enumerate(my_list) if my_list[num - 1] < my_list[num]]
-print(f'Исходный список {my_list}')
-print(f'Новый список {my_new_list}')
+print(f"Исходный список {my_list}")
+print(f"Новый список {my_new_list}")
